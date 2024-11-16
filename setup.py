b1576278cc30f35076051086a2ec5c25579a31ad
jakub627 <dybich89@gmail.com>
2024-11-16 14:41:44 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lab_tools",
-    version="0.1.14",
+    version="0.1.16",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.18.5",
